58b9fb2c6f180c17bd5b5370461eb729aff64ed1
user <user@email.com>
2025-02-20 06:59:00 -0500

    Agrega tabla para el historial

--- src/app/historial/pages/history/history.component.ts
+++ src/app/historial/pages/history/history.component.ts
@@ -1,15 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CardModule } from 'primeng/card';
 import { DividerModule } from 'primeng/divider';
 import { TableModule } from 'primeng/table';
+import { HistoryModels } from '../../../shared/models/history-models';
+import { LocalStorageService } from '../../../shared/services/local-storage.service';
+import { LocalStorageConstants } from '../../../shared/constants/local-storage.constants';
+import { CommonModule } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-history',
   standalone: true,
-  imports: [CardModule, DividerModule],
+  imports: [CardModule, DividerModule, TableModule, CommonModule, ButtonModule ],
   templateUrl: './history.component.html',
   styleUrl: './history.component.scss'
 })
-export class HistoryComponent {
+export class HistoryComponent implements OnInit{
+
+  historyItems: HistoryModels.Item[] = [];
+
+  constructor(private localStorageService: LocalStorageService){}
+
+  ngOnInit(): void {
+    const history = this.localStorageService.getItem<HistoryModels.Item[]>(LocalStorageConstants.history, true);
+    this.historyItems = history;
+    console.log(this.historyItems);
+  }
 
 }
