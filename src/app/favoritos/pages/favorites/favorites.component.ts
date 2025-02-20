927b0edeed37c0fdb922f035cf216ec8ba606880
user <user@email.com>
2025-02-20 09:30:10 -0500

    finaliza la implementación de favoritos

--- src/app/favoritos/pages/favorites/favorites.component.ts
+++ src/app/favoritos/pages/favorites/favorites.component.ts
@@ -8,6 +8,11 @@ import { ButtonModule } from 'primeng/button';
 import { CommonModule } from '@angular/common';
 import { TableModule } from 'primeng/table';
 import { TooltipModule } from 'primeng/tooltip';
+import { Router } from '@angular/router';
+import { RouterConstans } from '../../../shared/constants/router.constants';
+import { SelectedCityService } from '../../../shared/services/selected-city.service';
+import { ToastSeverityEnum } from '../../../shared/enums/toast-severity.enum';
+import { ToastService } from '../../../shared/services/toast.service';
 
 
 @Component({
@@ -21,7 +26,12 @@ export class FavoritesComponent implements OnInit {
 
   favorites : WeatherModels.CityInformation[];
 
-  constructor(private localStorageService: LocalStorageService){}
+  constructor(
+    private localStorageService: LocalStorageService,
+    private router: Router,
+    private selectedCityService: SelectedCityService,
+    private toastService: ToastService
+  ){}
 
 
   ngOnInit(): void {
@@ -33,6 +43,33 @@ export class FavoritesComponent implements OnInit {
     }
 
     this.favorites = list.reverse();
+  }
+
+  goToSearchPage(): void {
+    this.router.navigate([RouterConstans.weather]);
+  }
+  
+  searchCityAgain(city: string): void {
+    this.selectedCityService.setCity = city;
+    this.router.navigate([RouterConstans.weather]);
+  }
+
+  deleteCityFromFavorites(cityInformation: WeatherModels.CityInformation){
+    const list = this.localStorageService.getItem<WeatherModels.CityInformation[]>(LocalStorageConstants.favorites, true);
+
+    const newList = list.filter(
+      city => city.location.name !== cityInformation.location.name &&
+      city.location.region !== cityInformation.location.region
+    );
+
+    this.localStorageService.setItem<WeatherModels.CityInformation[]>(LocalStorageConstants.favorites, newList);
+
+    this.favorites = newList;
+
+    this.toastService.showToast(
+      ToastSeverityEnum.success,
+      `La ciudad ${cityInformation.location.name} se eliminó exitosamente de tus favoritos`
+    );
   }
 
 }
